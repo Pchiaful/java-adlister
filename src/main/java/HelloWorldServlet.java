b1b171f7c2aa19d1d4fcee8757fba8fbc74b72bd
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-11-22 05:32:11 -0600

    please just poooosh

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,21 +3,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 
-//Hey Tomcat, whenever someone requests the url /hello, load and execute this servlet
-//THe webservlet annotation maps a url to a servlet and its code
+// Hey Tomcat, whenever someone requests the URL /hello, load and execute this servlet
+// The WebServlet annotation maps a URL to a servlet and its code
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-//        out.println("<h1>Hello World!</h1>");
-        if (req.getParameter("name") != null)
         String name = req.getParameter("name");
-        out.println("<h1>Hello, " + name + "!</h1>");
+        if (name == null) {
+            out.println("<h1>Hello, World!</h1>");
+        } else {
+            out.println("<h1>Hello, " + name + "!</h1>");
+        }
     }
-
 }
