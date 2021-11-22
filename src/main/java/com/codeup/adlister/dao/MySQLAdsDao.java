152578c20e43c8bdca5a4ed3eb787b7974407d55
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-11-22 06:01:39 -0600

    final push

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -1,77 +1,77 @@
 package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
 import com.mysql.cj.jdbc.Driver;
+import sun.security.krb5.Config;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class MySQLAdsDao implements Ads {
+public class MySQLUsersDao implements Users {
     private Connection connection = null;
 
-    public MySQLAdsDao(Config config) {
+    public MySQLUsersDao(Config config) {
         try {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
-                config.getUrl(),
-                config.getUser(),
-                config.getPassword()
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
             );
-        } catch (SQLException e) {
-            throw new RuntimeException("Error connecting to the database!", e);
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
         }
     }
 
     @Override
-    public List<Ad> all() {
-        Statement stmt = null;
+    public User findByUsername(String username) {
         try {
-            stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
-            return createAdsFromResults(rs);
-        } catch (SQLException e) {
-            throw new RuntimeException("Error retrieving all ads.", e);
+            String safeSearchQuery = "SELECT * FROM users WHERE username = ?";
+
+            PreparedStatement ps = connection.prepareStatement(safeSearchQuery);
+
+            ps.setString(1, username);
+
+            return extractUser(ps.executeQuery());
+
+        } catch (SQLException sqlException) {
+            throw new RuntimeException(sqlException);
         }
     }
 
-    @Override
-    public Long insert(Ad ad) {
-        try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
-            ResultSet rs = stmt.getGeneratedKeys();
-            rs.next();
-            return rs.getLong(1);
-        } catch (SQLException e) {
-            throw new RuntimeException("Error creating a new ad.", e);
+    private User extractUser(ResultSet rs) throws SQLException {
+        if (!rs.next()) {
+            return null;
         }
-    }
-
-    private String createInsertQuery(Ad ad) {
-        return "INSERT INTO ads(user_id, title, description) VALUES "
-            + "(" + ad.getUserId() + ", "
-            + "'" + ad.getTitle() +"', "
-            + "'" + ad.getDescription() + "')";
-    }
-
-    private Ad extractAd(ResultSet rs) throws SQLException {
-        return new Ad(
-            rs.getLong("id"),
-            rs.getLong("user_id"),
-            rs.getString("title"),
-            rs.getString("description")
+        return new User (
+                rs.getLong("id"),
+                rs.getString("username"),
+                rs.getString("email"),
+                rs.getString("password")
         );
     }
 
-    private List<Ad> createAdsFromResults(ResultSet rs) throws SQLException {
-        List<Ad> ads = new ArrayList<>();
-        while (rs.next()) {
-            ads.add(extractAd(rs));
+    @Override
+    public Long insert(User user) {
+        try {
+            String insertQuery = String.format("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+
+            PreparedStatement ps = connection.prepareStatement(insertQuery, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            rs.next();
+
+            return rs.getLong(1);
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating a new user.", e);
         }
-        return ads;
     }
 }
